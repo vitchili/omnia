c4bb96e454d68b37028bf7f56ee5c3108472a0e1
Vitor Pimenta <vitor.pimenta.dev@gmail.com>
2026-06-02 03:43:53 -0300

    Remove pt-bt unnecessary comments

--- template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
@@ -10,14 +10,11 @@ public class CreateSaleProfile : Profile
 {
     public CreateSaleProfile()
     {
-        // Mapeamentos de entrada (Command para as Entidades do Domínio)
-        // Nota: Mapeia a lista 'Products' do Command para a propriedade correspondente 'Items' da Entidade se aplicável
         CreateMap<CreateSaleCommand, Sale>()
             .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Products));
             
         CreateMap<CreateSaleProductCommand, SaleItem>();
 
-        // Mapeamentos de saída (Entidades do Domínio para o Result do MediatR)
         CreateMap<Sale, CreateSaleResult>()
             .ForMember(dest => dest.Products, opt => opt.MapFrom(src => src.Items));
             
